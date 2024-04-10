e5e235dacfb089e3023a19e98efe24307a1a70e6
Harsh Garg <harshgargckt@gmail.com>
2024-04-10 10:19:35 +0530

    first steps in react

---
+++ src/nine.js
@@ -0,0 +1,26 @@
+import React from 'react'
+import './nine.css';
+
+export const nineCircles = () => {
+  return (
+    <div>
+
+    <h1>Circles</h1>
+
+    <div class="Box">
+        <div class="Key"></div>
+        <div class="Key"></div>
+        <div class="Key"></div>
+        <div class="Key"></div>
+        <div class="Key"></div>
+        <div class="Key"></div>
+        <div class="Key"></div>
+        <div class="Key"></div>
+        <div class="Key"></div>
+    </div>
+
+
+
+    </div>
+  )
+}
